7bd68e4c09a804a3283515f9a8272b287b636238
xiaodong.li@huangjinqianbao.com <xiaodong.li@huangjinqianbao.com>
2018-05-14 16:56:35 +0800

    最长回文字符串

--- src/algorithm/quickSort.java
+++ src/algorithm/quickSort.java
@@ -5,39 +5,37 @@ import java.util.ArrayList;
 import java.util.Arrays;
 
 public class quickSort {
-    public static void quickSortTest(Integer[] arr,Integer start,Integer end){
-        System.out.println("result="+Arrays.toString(arr));
-        int base=arr[start];
-        int i =start;
-        int j =end;
-        if (j>i){
-            while (arr[i]<base&&i<end){
-                i++;
-            }
-            while (base<arr[j]&&j>start){
+    public static void quickSortTest(int[] arr,Integer low,Integer high){
+        if (low>high){
+            return;
+        }
+        int i=low,j=high;
+        int base=arr[low];
+        while (i<j){
+            while (i<j&&base<arr[j]){
                 j--;
+            }
+            while (i<j&&base>=arr[i]){
+                i++;
             }
             if (i<j){
-                int temp=arr[i];
-                arr[i]=arr[j];
-                arr[j]=temp;
-                i++;
-                j--;
-            }
-
-            if (i<end){
-                quickSortTest(arr,i+1,end);
-            }
-
-            if (j>start){
-                quickSortTest(arr,start,j-1);
+                swap(arr,i,j);
             }
         }
+        swap(arr,low,i);
+        quickSortTest(arr,low,i-1);
+        quickSortTest(arr,i+1,high);
     }
 
+    public static void swap(int[] arr,int start,int end){
+        int temp=arr[start];
+        arr[start]=arr[end];
+        arr[end]=temp;
+    }
     public static void main(String[] args) {
-        Integer[] testArr = new Integer[]{0, 1, 13, 25, 36, 17, 18, 2};
+        int[] testArr = new int[]{ 3,36, 25, 10, 1, 18,17,  21};
         quickSortTest(testArr,0,testArr.length-1);
         System.out.println(Arrays.toString(testArr));
+        //
     }
 }
