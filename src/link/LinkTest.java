6deb6197ab4dd5b6e403e0ecf4ac74b77777dd39
lxd <768265051@qq.com>
2018-10-16 17:45:55 +0800

    检测链表有环 并且找出相交的节点

--- src/link/LinkTest.java
+++ src/link/LinkTest.java
@@ -1,5 +1,8 @@
 package link;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class LinkTest {
 
     private Node header;
@@ -48,120 +51,106 @@ public class LinkTest {
         int n = 2;
 //        Node temp1 = deletePointNode(link1.header,2);
 //        Node temp1 = reserveLink(link.header,n);
-//        Node temp1 =reverse(link.header);
+//        Node temp1 =reverse1(link1.header);
 //        Node temp1 = findLastOne(link.header);
 //        Node temp1 = findMidNode(link1.header);
 
 //        printNode(temp1);
-        System.out.println(checkLinkCycle(link1.header));
+        Boolean isCycle=checkCycle(link1.header);
+
+        Node node = getMeetNod(link1.header);
+
+        System.out.println(node.value);
 
     }
 
-    private static void printNode(Node temp1){
-                while (temp1!=null){
-            System.out.println(temp1.value);
-            temp1 = temp1.next;
-
+    private static Node getMeetNod(Node header) {
+        Map map = new HashMap();
+        while (header != null) {
+            if (map.containsKey(header)){
+                return header;
+            }else {
+                map.put(header,header.value);
+            }
+            header = header.next;
         }
+        return null;
     }
 
-    static boolean checkLinkCycle(Node node){
+
+    private static Boolean checkCycle(Node node){
         Node fast = node;
         Node slow = node;
         while (fast!=null&&fast.next!=null){
-            fast=fast.next.next;
-            slow=slow.next;
-            if (slow == fast){
+            fast = fast.next.next;
+            slow = slow.next;
+            if (fast==slow){
                 return true;
             }
         }
         return false;
     }
 
-    //a-b-c-d-e
-    private static Node findMidNode(Node node){
-        Node fast=node;
-        Node slow = node;
-
-        while (fast!=null&&fast.next!=null){
-            fast=fast.next.next;
-            slow=slow.next;
+    private static Node deletePointNode(Node node, int n) {
+        Node temp = node;
+        for (int i = 1; i < n-1; i++) {
+            node = node.next;
         }
-        slow.next=null;
+        node.next = node.next.next;
+        return temp;
+    }
+
+    private static Node findMidNode(Node node){
+        if (node==null)return null;
+        Node fast = node;
+        Node slow = node;
+        while (fast!=null&&fast.next!=null){
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+        slow.next =null;
         return slow;
     }
 
 
-    private static Node deletePointNode(Node node, int n) {
-        Node temp = node;
-        for (int i = 0; i<n -1; i++) {
-            if (temp.next!=null){
-                temp=temp.next;
-            }
-        }
-        if (temp.next==null){
-            return null;
-        }
-        temp.next=temp.next.next;
+    private static Node mergeLink(Node node1,Node node2){
+        if (node1==null) return node2;
+        if (node2==null) return node1;
 
-        return node;
-    }
-
-    private static Node mergeLink(Node node1, Node node2) {
-        Node result=null;
-        if (node1==null)return node2;
-        if (node2==null)return node1;
+        Node pre = null;
         if (node1.value>=node2.value){
-            result=node2;
-            node2.next = mergeLink(node2.next,node1);
+            pre=node2;
+            pre.next=mergeLink(node1,node2.next);
         }else {
-            result = node1;
-            node1.next =mergeLink(node1.next,node2);
+            pre=node1;
+            pre.next= mergeLink(node1.next,node2);
         }
-        return result;
+        return pre;
     }
 
-
-    // a-b-c-d-e
-    //递归
-    public static Node findLastOne(Node node){
+    //非递归反转
+    private static Node reverse1(Node node) {
         Node pre = null;
-        if (node == null||node.next == null) {
-            pre =node;
+        if (node == null || node.next == null){
+            pre = node;
         }else {
-            Node temp = findLastOne(node.next);
+            Node temp = reverse1(node.next);
             node.next.next=node;
-            node.next=null;
-            pre = temp;
-        }
-
-        return pre;
-    }
-
-
-    //非递归
-    public static Node reverse(Node node) {
-        Node pre= null;
-        while (node!=null){
-            Node temp = node;
-            node = node.next;
-            temp.next=pre;
-            pre=temp;
+            node.next = null;
+            pre =temp;
         }
         return pre;
     }
 
-    //非递归1
-    private static Node reserveLink(Node node, int n) {
-        Node pre = null;
-        while (node!=null){
-            Node temp = node.next;
-            node.next=pre;
-            pre=node;
-            node = temp;
+    private static void printNode(Node temp1){
+        while (temp1 != null) {
+            System.out.println(temp1.value);
+            temp1 = temp1.next;
+
         }
-        return pre;
     }
+
+
 
     private static LinkTest createLink() {
         LinkTest link = new LinkTest();
@@ -170,15 +159,15 @@ public class LinkTest {
         Node node3 = link.createNode(5);
         Node node4 = link.createNode(7);
         Node node5 = link.createNode(9);
-//        Node node6 = link.createNode(11);
+        Node node6 = link.createNode(11);
 
         link.addNode(node1);
         link.addNode(node2);
         link.addNode(node3);
         link.addNode(node4);
         link.addNode(node5);
-        node5.next=node1;
-//        link.addNode(node6);
+        link.addNode(node6);
+        node6.next=node3;
         return link;
     }
     private static LinkTest createLink1() {
