d435a1a6d12f5a9b611651dc235c2d6ea5a11ff0
lxd <768265051@qq.com>
2018-10-18 15:40:10 +0800

    插入排序

--- src/sort/BubbleSort.java
+++ src/sort/BubbleSort.java
@@ -3,12 +3,12 @@ package sort;
 import java.util.Arrays;
 
 public class BubbleSort {
-    static int[] nums = new int[]{8,3,9,4,5,1,2,11};
+    static int[] nums = new int[]{11,22,9,8,4,5,1,2};
 
 
     private static void bubbleSort(){
         for (int i = 0; i < nums.length-1; i++) {
-            for (int j = 0; j <nums.length-2; j++) {
+            for (int j = 0; j <nums.length-i-1; j++) {
                 if (nums[j]>nums[j+1]){
                     int temp = nums[j];
                     nums[j]=nums[j+1];
@@ -18,10 +18,23 @@ public class BubbleSort {
         }
     }
 
+    private static void insertSort(){
+        int length = nums.length;
+        for (int i = 1; i < length; i++) {
+            int value = nums[i];
+            for (int j = i-1; j >=0; j--) {
+                if (value<=nums[j]){
+                    nums[j+1] = nums[j];
+                    nums[j]=value;
+                }
+            }
+        }
+    }
+
 
     public static void main(String[] args) {
-        bubbleSort();
-
+//        bubbleSort();
+        insertSort();
         System.out.println( Arrays.toString(nums));
     }
 }
