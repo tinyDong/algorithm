571b9c23f15c51bd9945b670e2d53b8bf901e05f
lxd <768265051@qq.com>
2018-11-12 18:05:40 +0800

    动态规划 金矿问题

--- src/Array/ArrayTest.java
+++ src/Array/ArrayTest.java
@@ -11,7 +11,118 @@ import java.util.Arrays;
 //所以返回 [0, 1]
 public class ArrayTest {
 
-    public static final int[] nums=new int[]{2, 7, 11, 15};
+    public static final int[] nums1=new int[]{1, 2};
+    public static final int[] nums2=new int[]{3, 4};
+
+
+    public static void main(String[] args) {
+//        System.out.println(Arrays.toString(findTargetNumbers(nums,18)));
+//        System.out.println(findMedianSortedArrays(nums1,nums2));
+    }
+
+
+//    给定两个大小为 m 和 n 的有序数组 nums1 和 nums2 。
+//
+//    请找出这两个有序数组的中位数。要求算法的时间复杂度为 O(log (m+n)) 。
+//
+//    示例 1:
+//    你可以假设 nums1 和 nums2 不同时为空。
+//    nums1 = [1, 3]
+//    nums2 = [2]
+//
+//    中位数是 2.0
+//    示例 2:
+//    nums1 = [1, 2,3]
+//    nums2 = [3, 4]
+//
+//    中位数是 (2 + 3)/2 = 2.5
+
+
+
+
+//    int findMedian_logn(int vec1[], int n1, int vec2[], int n2)
+//    {
+//        int m1 = (n1-1) / 2, m2 = (n2-1) / 2;
+//        if(n1 == 1)
+//        {//递归结束条件
+//            if(n2 == 1)
+//                return vec1[0] < vec2[0] ? vec1[0] : vec2[0];
+//            if(n2 % 2 == 0)
+//            {
+//                if(vec1[0] >= vec2[m2+1])
+//                    return vec2[m2+1];
+//                else if(vec1[0] <= vec2[m2])
+//                    return vec2[m2];
+//                else return vec1[0];
+//            }
+//            else
+//            {
+//                if(vec1[0] >= vec2[m2])
+//                    return vec2[m2];
+//                else if(vec1[0] <= vec2[m2-1])
+//                    return vec2[m2-1];
+//                else return vec1[0];
+//            }
+//        }
+//        else if(n2 == 1)
+//        {//递归结束条件
+//            if(n1 % 2 == 0)
+//            {
+//                if(vec2[0] >= vec1[m1+1])
+//                    return vec1[m1+1];
+//                else if(vec2[0] <= vec1[m1])
+//                    return vec1[m1];
+//                else return vec2[0];
+//            }
+//            else
+//            {
+//                if(vec2[0] >= vec1[m1])
+//                    return vec1[m1];
+//                else if(vec2[0] <= vec1[m1-1])
+//                    return vec1[m1-1];
+//                else return vec2[0];
+//            }
+//        }
+//        else
+//        {
+//            int cutLen = n1/2 > n2/2 ? n2/2 : n1/2;//注意每次减去短数组的一半，如果数组长度n是奇数，一半是指n-1/2
+//            if(vec1[m1] == vec2[m2])return vec1[m1];
+//            else if(vec1[m1] < vec2[m2])
+//                return findMedian_logn(&vec1[cutLen], n1-cutLen, vec2, n2-cutLen);
+//        else
+//            return findMedian_logn(vec1, n1-cutLen, &vec2[cutLen], n2-cutLen);
+//        }
+//    }
+
+    private static double findMidNum(int[] nums1) {
+        double result = 0.0;
+        if (nums1.length%2==0){
+            result= (nums1[nums1.length/2] + nums1[nums1.length/2-1])/2.0;
+        }else {
+            result = nums1[nums1.length/2];
+        }
+        return result;
+    }
+
+    private static void merge(int[] nums1, int[] nums2, int[] temp) {
+//        int low = 0 ;
+//        int high = 0;
+//        int k =0;
+//        while (low<=nums1.length-1&&high<=nums2.length-1){
+//            if (nums1[low]<=nums2[high]){
+//                temp[k++]=nums1[low++];
+//            }else {
+//                temp[k++] = nums2[high++];
+//            }
+//        }
+//        while (low<=nums1.length-1){
+//            temp[k++]=nums1[low++];
+//        }
+//        while (high<= nums2.length-1){
+//            temp[k++]=nums2[high++];
+//        }
+    }
+
 
     public static int[] findTargetNumbers(int[] arr,int target){
 
@@ -28,9 +139,5 @@ public class ArrayTest {
             }
         }
         return null;
-    }
-
-    public static void main(String[] args) {
-        System.out.println(Arrays.toString(findTargetNumbers(nums,18)));
     }
 }
---
+++ src/dynamicProgramming/dynamicTest.java
@@ -0,0 +1,86 @@
+package dynamicProgramming;
+
+public class dynamicTest {
+
+
+//    https://www.sohu.com/a/153858619_466939
+    public static void main(String[] args) {
+
+        int result= getMostGold(5,10,new int[]{400,300,550,300,200},new int[]{5,5,2,4,3});
+
+        System.out.println(result);
+
+    }
+
+
+    public static int getMostGold(int n , int w ,int[] g,int[] p){
+
+        int[] preResults = new int[w];
+        int[] results = new int[w];
+
+        for (int i = 0; i < w; i++) {
+            if (i+1 <p[0]){
+                preResults[i] = 0;
+            }else {
+                preResults[i] = g[0];
+            }
+        }
+
+        for (int i = 1; i < n; i++) {
+
+            for (int j = 0; j < w; j++) {
+                if (j +1< p[i]){
+                    results[j] = preResults[j];
+                }else if (j+1 == p[i]){
+                    results[j] = Math.max(preResults[j],preResults[j-p[i]+1]+g[i]);
+                }
+                else{
+                    results[j] = Math.max(preResults[j],preResults[j-p[i]]+g[i]);
+                }
+            }
+            System.arraycopy(results, 0, preResults, 0, preResults.length);
+        }
+
+        return results[w-1];
+    }
+
+
+    static int Method_1(int n,int w,int [] m,int [] p)
+    {
+        int[] preResult = new int[w];
+        int[] result = new int[w];
+        for(int i = 0; i <p.length; i++)
+        {
+            if (w < p[i])
+            {
+                preResult[i] = 0;
+            }
+            else
+            {
+                preResult[i] = m[i];
+            }
+        }
+
+
+
+        for(int i = 0; i < n; i++)//金矿
+        {
+            for(int j = 0; j < w; j++)//挖矿工数
+            {
+                if (j < p[i])
+                {
+                    result[j] = preResult[j];
+                }
+                else
+                {
+                    result[j] = (preResult[j - p[i]] + m[i]) > preResult[j] ? (preResult[j - p[i]] + m[i]) : preResult[j];
+                }
+            }
+            preResult = result;
+        }
+        return result[n-1];
+
+
+    }
+
+}
