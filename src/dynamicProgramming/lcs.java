23c15e88258caade17f86610c57ff48f1d05d3b6
lxd <768265051@qq.com>
2018-11-15 10:38:22 +0800

    动态规划 最长公共子序列 第一次练习

--- src/dynamicProgramming/lcs.java
+++ src/dynamicProgramming/lcs.java
@@ -18,45 +18,46 @@ public class lcs {
 
     }
 
-    private static int getLCS(char[] char1, char[] char2){
+    private static void getLCS(char[] char1, char[] char2){
+
         int[][] dp = new int[char1.length+1][char2.length+1];
-        int[] maxSubStr = new int[Math.max(char1.length,char2.length)];
-        for (int i = 0; i <=char1.length; i++) {
+
+        for (int i = 0; i <= char1.length; i++) {
             for (int j = 0; j <= char2.length; j++) {
                 if (i==0||j==0){
                     dp[i][j]=0;
                 }else if (char1[i-1]==char2[j-1]){
-                    dp[i][j]=dp[i-1][j-1]+1;
+                    dp[i][j] = dp[i-1][j-1]+1;
                 }else {
                     dp[i][j] = Math.max(dp[i-1][j],dp[i][j-1]);
                 }
             }
         }
+        System.out.println("最长长度"+dp[char1.length][char2.length]);
+
 
         Stack stack = new Stack();
-        int i = char1.length - 1;
-        int j = char2.length - 1;
+
+
+        int i = char1.length-1;
+        int j = char2.length-1;
 
         while (i>=0&&j>=0){
-            if (char1[i] == char2[j]){
+            if (char1[i]==char2[j]){
                 stack.push(char1[i]);
                 i--;
                 j--;
             }else {
-                //如果字符串的字符不同，则在数组中找相同的字符，注意：数组的行列要比字符串中字符的个数大1，因此i和j要各加1
-                if (dp[i][j+1]>dp[i+1][j]){
+                if (dp[i+1][j]>dp[i][j+1]){
                     i--;
                 }else {
                     j--;
                 }
             }
         }
-
-        while(!stack.isEmpty()){//打印输出栈正好是正向输出最大的公共子序列
-            System.out.print(stack.pop());
+        while (!stack.isEmpty()){
+            System.out.println(stack.pop());
         }
-
-        return dp[char1.length][char2.length];
     }
 
 }
