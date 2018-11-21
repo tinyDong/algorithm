34d6bc2945ada2c2ad456b16207ce63318da07f4
lxd <768265051@qq.com>
2018-11-21 15:13:36 +0800

    最长公共子序列练习 1

--- src/dynamicProgramming/lcs.java
+++ src/dynamicProgramming/lcs.java
@@ -20,45 +20,44 @@ public class lcs {
 
     private static void getLCS(char[] char1, char[] char2){
 
-        int[][] dp = new int[char1.length+1][char2.length+1];
+        int[][] dp =new int[char1.length+1][char2.length+1];
 
         for (int i = 0; i <= char1.length; i++) {
-            for (int j = 0; j <= char2.length; j++) {
+            for (int j = 0; j <=char2.length; j++) {
                 if (i==0||j==0){
                     dp[i][j]=0;
                 }else if (char1[i-1]==char2[j-1]){
-                    dp[i][j] = dp[i-1][j-1]+1;
+                    dp[i][j]= dp[i-1][j-1]+1;
                 }else {
-                    dp[i][j] = Math.max(dp[i-1][j],dp[i][j-1]);
+                    dp[i][j]=Math.max(dp[i-1][j],dp[i][j-1]);
                 }
             }
         }
-        System.out.println("最长长度"+dp[char1.length][char2.length]);
 
+        System.out.println(dp[char1.length][char2.length]);
 
-        Stack stack = new Stack();
+        Stack stack=new Stack();
 
-
-        int i = char1.length-1;
-        int j = char2.length-1;
-
+        int i = char1.length-1 ;
+        int j = char2.length-1 ;
         while (i>=0&&j>=0){
             if (char1[i]==char2[j]){
                 stack.push(char1[i]);
+                j--;
                 i--;
+            }else if (dp[i+1][j]>dp[i][j+1]){
                 j--;
             }else {
-                if (dp[i+1][j]>dp[i][j+1]){
-                    i--;
-                }else {
-                    j--;
-                }
+                i--;
             }
         }
         while (!stack.isEmpty()){
             System.out.println(stack.pop());
         }
+
     }
+
+
 
 }
 
@@ -73,18 +72,3 @@ public class lcs {
 
 
 
-
-//        while((i >= 0) && (j >= 0)){
-//                if(char1[i] == char2[j]){//字符串从后开始遍历，如若相等，则存入栈中
-//                stack.push(char1[i]);
-//                i--;
-//                j--;
-//                }else{
-//                //如果字符串的字符不同，则在数组中找相同的字符，注意：数组的行列要比字符串中字符的个数大1，因此i和j要各加1
-//                if(dp[i+1][j] > dp[i][j+1]){
-//                j--;
-//                }else{
-//                i--;
-//                }
-//                }
-//                }
