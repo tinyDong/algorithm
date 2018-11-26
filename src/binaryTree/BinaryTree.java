97c9c2b00e992fbefc8fd05a0f90ddb3ebfc9f76
lxd <768265051@qq.com>
2018-11-26 14:35:33 +0800

    二叉树前中后递归 非递归遍历练习1

--- src/binaryTree/BinaryTree.java
+++ src/binaryTree/BinaryTree.java
@@ -1,6 +1,6 @@
 package binaryTree;
 
-import java.util.*;
+import java.util.Stack;
 
 public class BinaryTree {
 
@@ -52,136 +52,19 @@ public class BinaryTree {
     public void visited(TreeNode node){
         if (node==null)return;
         node.isVisted=true;
-        System.out.println("====="+node.key+"----"+node.data);
-    }
-    //前序遍历
-    public void preOrder(TreeNode root){
-        if (root!=null){
-            visited(root);
-            preOrder(root.leftChild);
-            preOrder(root.rightChild);
-        }
+        System.out.println("=="+node.key+"----"+node.data);
     }
 
-//    *           A
-//    *     B          C
-//    *  D     E            F
-    //前序遍历的非递归实现
-    public void nonRecPreOrder(TreeNode root) {
-        if (root==null)return;
-        Stack<TreeNode> stack = new Stack<>();
-        TreeNode temp = root;
-        while (temp!= null||!stack.isEmpty()){
-            while (temp!=null){
-                stack.push(temp);
-                visited(temp);
-                temp = temp.leftChild;
-            }
-            temp = stack.pop();
-            temp = temp.rightChild;
-        }
-    }
-
-    //中序遍历
-    public void midOrder(TreeNode root){
-        if (root!=null){
-            midOrder(root.leftChild);
-            visited(root);
-            midOrder(root.rightChild);
-        }
-    }
-//    *           A
-//     *     B          C
-//     *  D     E            F
-    //中序遍历的非递归实现
-    public void noRecMidOrder(TreeNode root){
-        if (root==null)return;
-        Stack<TreeNode> stack =new Stack<>();
-        TreeNode temp = root;
-        while (temp !=null || !stack.isEmpty()){
-            while (temp!=null){
-                stack.push(temp);
-                temp = temp.leftChild;
-            }
-            temp = stack.pop();
-            visited(temp);
-            temp = temp.rightChild;
-        }
-
-    }
-
-
-    //后序遍历
-    public void postOrder(TreeNode root){
-        if (root!=null){
-            postOrder(root.leftChild);
-            postOrder(root.rightChild);
-            visited(root);
-        }
-    }
-
-    /**
-     * 创建一棵二叉树
-     * <pre>
-     *           A
-     *     B          C
-     *  D     E            F
-     *  </pre>
-     * @param root
-     * @author WWX
-     */
-    public void nonRecPostOrder(TreeNode root){
-        if (root==null) return;
-        Stack<TreeNode> stack = new Stack<>();
-        Stack<TreeNode> resultStack = new Stack<>();
-        TreeNode temp = root;
-        while (temp != null || !stack.isEmpty()){
-            if (temp != null){
-                stack.push(temp);
-                resultStack.push(temp);
-                temp = temp.rightChild;
-            }else {
-                temp = stack.pop();
-                temp = temp.leftChild;
-            }
-        }
-        while (!resultStack.isEmpty()){
-            visited(resultStack.pop());
-        }
-    }
-
-    public int level(TreeNode root) {
-        if (root == null)
-            return 0;
-        return level(root.leftChild) + 1 > level(root.rightChild) + 1 ? level(root.leftChild) + 1: level(root.rightChild) + 1;
-    }
-
-
-    public void layerOrder(TreeNode root){
-        if (root==null){
-            return;
-        }
-        Queue<TreeNode> queue = new ArrayDeque<>();
-        TreeNode temp = root;
-        queue.add(root);
-        while (!queue.isEmpty()){
-            temp = queue.peek();
-            visited(temp);
-            if (temp.leftChild!=null){
-                queue.add(temp.leftChild);
-            }
-            if (temp.rightChild !=null){
-                queue.add(temp.rightChild);
-            }
-            queue.poll();
-        }
-    }
 
 
         public static void main(String[] args) {
         BinaryTree bT=new BinaryTree();
         bT.createBinaryTree(bT.root);
 //        bT.preOrder(bT.root);
+//        System.out.println("=========");
+//        bT.midOrder(bT.root);
+//        System.out.println("=========");
+//        bT.afterOrder(bT.root);
 //        System.out.println("========");
 //        bT.nonRecPreOrder(bT.root);
 
@@ -189,10 +72,101 @@ public class BinaryTree {
 //        System.out.println("========");
 //        bT.noRecMidOrder(bT.root);
 //
-//        bT.postOrder(bT.root);
-//        System.out.println("========");
-//        bT.nonRecPostOrder(bT.root);
-        bT.layerOrder(bT.root);
+        bT.afterOrder(bT.root);
+        System.out.println("========");
+        bT.nonRecPostOrder(bT.root);
+//        bT.layerOrder(bT.root);
 //        System.out.println(bT.level(bT.root));
     }
+
+    private void nonRecPostOrder(TreeNode header) {
+        if (header==null){
+            return;
+        }
+
+        Stack<TreeNode> stack = new Stack<>();
+        Stack<TreeNode> resultStack = new Stack<>();
+        stack.push(header);
+        while (!stack.isEmpty()){
+            header = stack.pop();
+            resultStack.push(header);
+            if (header.leftChild!=null){
+                stack.push(header.leftChild);
+            }
+            if (header.rightChild!=null){
+                stack.push(header.rightChild);
+            }
+        }
+        while (!resultStack.isEmpty()){
+            visited(resultStack.pop());
+        }
+
+    }
+
+    private void noRecMidOrder(TreeNode header) {
+        if (header==null){
+            return;
+        }
+        Stack<TreeNode> stack = new Stack<>();
+        while (!stack.isEmpty()||header!=null){
+            if (header!=null){
+                stack.push(header);
+                header = header.leftChild;
+            }else {
+                header = stack.pop();
+                visited(header);
+                header = header.rightChild;
+            }
+        }
+    }
+
+
+
+    private void nonRecPreOrder(TreeNode header) {
+        if (header==null){
+            return;
+        }
+        Stack<TreeNode> stack = new Stack<>();
+        stack.push(header);
+        while (!stack.isEmpty()){
+            header = stack.pop();
+            visited(header);
+            if (header.rightChild!=null){
+                stack.push(header.rightChild);
+            }
+            if (header.leftChild!=null){
+                stack.push(header.leftChild);
+            }
+        }
+    }
+
+
+    private void preOrder(TreeNode root) {
+        if (root ==null){
+            return;
+        }
+        visited(root);
+        preOrder(root.leftChild);
+        preOrder(root.rightChild);
+    }
+
+    private void midOrder(TreeNode root) {
+        if (root ==null){
+            return;
+        }
+        midOrder(root.leftChild);
+        visited(root);
+        midOrder(root.rightChild);
+    }
+
+    private void afterOrder(TreeNode root) {
+        if (root ==null){
+            return;
+        }
+        afterOrder(root.leftChild);
+        afterOrder(root.rightChild);
+        visited(root);
+    }
+
+
 }
