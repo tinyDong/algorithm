aa43555ad7c47c23a4c222dddb0631dbc2456112
lxd <768265051@qq.com>
2018-11-27 14:55:14 +0800

    变态青蛙跳

--- src/jianzhioffer/code10.java
+++ src/jianzhioffer/code10.java
@@ -1,30 +1,32 @@
 package jianzhioffer;
 
-//求斐波那契数列的第 n 项
+
+import java.util.Arrays;
+
 public class code10 {
 
     public static void main(String[] args) {
 
-
-        int a =0 ;
-        int b =1 ;
-        //0 1 1 2 3 5
-        int result =0;
-        for (int i = 0; i <= 5; i++) {
-            result = a+b;
-            a=b;
-            b=result;
-
-        }
-
-        System.out.println(result);
-
         System.out.println("====");
 
-        System.out.println(getResult(7));
+        System.out.println(jumpMax(3));
 
     }
 
+//    青蛙一次可以跳上 1 级台阶，也可以跳上 2 级... 它也可以跳上 n 级。求该青蛙跳上一个 n 级的台阶总共有多少种跳法。
+    private static int jumpMax(int target){
+            int[] dp = new int[target];
+        Arrays.fill(dp, 1);
+        for (int i = 1; i < target; i++){
+            for (int j = 0; j < i; j++) {
+                dp[i] = dp[i] + dp[j];
+            }
+        }
+        return dp[target-1];
+    }
+
+
+    //求斐波那契数列的第 n 项
     private static  int getResult(int n ){
         if (n <= 1)
             return n;
@@ -37,4 +39,33 @@ public class code10 {
         }
         return fib;
     }
+
+//    //一只青蛙一次可以跳上 1 级台阶，也可以跳上 2 级。求该青蛙跳上一个 n 级的台阶总共有多少种跳法。
+//    private static int jump(int n){
+//
+//        int f1 = 1;
+//        int f2 = 2;
+//        int result=0;
+//        for (int i = 3; i <=n ; i++) {
+//            result = f1+f2;
+//            f1 = f2;
+//            f2 = result;
+//
+//        }
+//        return result;
+//    }
+//
+//    //我们可以用 2*1 的小矩形横着或者竖着去覆盖更大的矩形。请问用 n 个 2*1 的小矩形无重叠地覆盖一个 2*n 的大矩形，总共有多少种方法？
+//    private static int getMax(int i) {
+//
+//        int f1 = 1;
+//        int f2 = 2;
+//        int result = 0;
+//        for (int j = 3; j <=i ; j++) {
+//            result = f1+f2;
+//            f1 = f2;
+//            f2 =result;
+//        }
+//        return result;
+//    }
 }
