11af69127b9e488cf5881608e3c8908f3a171fe9
lxd <768265051@qq.com>
2018-12-04 21:17:47 +0800

    leetCode 练习

---
+++ src/leetCode/code1.java
@@ -0,0 +1,28 @@
+package leetCode;
+//两数之和
+//给定 nums = [2, 7, 11, 15], target = 9
+//因为 nums[0] + nums[1] = 2 + 7 = 9
+//所以返回 [0, 1]
+public class code1 {
+
+    public static void main(String[] args) {
+        int[] nums =new int[]{2, 7, 11, 15};
+
+        int i = 0 ;
+        int j =nums.length - 1;
+        int target = 9;
+        while (i<j){
+            int result =  nums[i] + nums[j];
+            if (result==target){
+                System.out.println("i="+i+"j"+j);
+                return;
+            }
+            if (result>target){
+                j--;
+            }
+            if (result<target){
+                i++;
+            }
+        }
+    }
+}
---
+++ src/leetCode/code7.java
@@ -0,0 +1,55 @@
+package leetCode;
+
+public class code7 {
+
+    public static void main(String[] args) {
+        System.out.println(reverse(-100234));
+    }
+
+//    static void reverse(int a) {
+//        int rs = 0;
+//
+//        while (a > 0) {
+//            rs = rs* 10;
+//            rs =rs + a % 10;
+//            a = a/10;
+//        }
+//        System.out.println(rs);
+//    }
+    public static int reverse(int x) {
+        if (x == Integer.MIN_VALUE){
+            return x;
+        }
+        int res = 0;
+        int num = Math.abs(x);
+        while (num!=0){
+            if (res>Integer.MAX_VALUE){
+                return x>0? Integer.MAX_VALUE:Integer.MIN_VALUE;
+            }
+            res =res*10 +num%10;
+            num = num/10;
+        }
+        return x>0?res:-res;
+    }
+
+
+
+
+
+
+
+
+
+//       if (x == Integer.MIN_VALUE)
+//            return Integer.MIN_VALUE;
+//    int num = Math.abs(x);
+//    int res = 0;
+//        while (num != 0) {
+//        if (res > Integer.MAX_VALUE) {
+//            return x > 0 ? Integer.MAX_VALUE : Integer.MIN_VALUE;
+//        }
+//        res = res * 10 + num % 10;
+//        num = num/10;
+//    }
+//        return x > 0 ? res : -res;
+}
