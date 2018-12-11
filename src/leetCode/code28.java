e85f83421011b0034879c56da34d1e4b977f7332
lxd <768265051@qq.com>
2018-12-11 10:42:46 +0800

    leetCode 28练习

--- src/leetCode/code28.java
+++ src/leetCode/code28.java
@@ -18,32 +18,23 @@ public class code28 {
 
     // KMP算法
     public static int strStr3(String haystack, String needle) {
-        if(haystack == null || needle == null || haystack.length() < needle.length())
-            return -1;
-        if(haystack.length() == 0 || needle.length() == 0)
-            return 0;
-        int[] next = getNext(needle);
-        int hlen = haystack.length();
-        int nlen = needle.length();
-        int hindex = 0;
-        int nindex = 0;
-        while(hindex < hlen && nindex < nlen)
-        {
-            if(haystack.charAt(hindex) == needle.charAt(nindex)) // 从头开始，若匹配就一直继续比较
-            {
-                hindex ++;
-                nindex ++;
-            }
-            else if(next[nindex] == -1)  // 表示与子串的第一个字符就没有匹配上，则主串自己向后移动
-            {
-                hindex ++;
-            }
-            else   // 直到nindex - 1都匹配上了，所以子串不需要从头继续，只需要从next[nindex]位继续匹配即可
-            {
-                nindex = next[nindex];
+
+        char[] char1 = haystack.toCharArray();
+        char[] char2 = needle.toCharArray();
+
+        int i =0;
+        int j = 0;
+
+        while (i<char1.length&&j<char2.length){
+            if (char1[i]==char2[j]){
+                i++;
+                j++;
+            }else {
+                i = i-j + 1;
+                j=0;
             }
         }
-        return nindex == nlen ? hindex - nindex : -1; // 如果匹配到子串的结尾，则返回此时主串与子串的位置差，即为开始匹配的地方
+        return j==char2.length ? i-j:-1;
     }
 
     public static int[] getNext(String ps) {
