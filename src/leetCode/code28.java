a43c781f6033fb6c67ffebc14335c0f015456c25
lxd <768265051@qq.com>
2018-12-12 10:37:20 +0800

    leetCode 28kmp练习

--- src/leetCode/code28.java
+++ src/leetCode/code28.java
@@ -15,21 +15,21 @@ package leetCode;
 //https://www.zhihu.com/search?type=content&q=kmp
 public class code28 {
     public static void main(String[] args) {
-//        System.out.println(Arrays.toString(getNext("abab")));
-        System.out.println(strStr3("BBC ABCDAB ABCDABDABDE","ABCDABD"));
+
+        String haystack = "aaaaa", needle = "bba";
+//        String haystack = "BBC ABCDAB ABCDABDABDE", needle = "ABCDABD";
+        System.out.println(strStr3(haystack,needle));
 
     }
 
     private static int strStr3(String haystack, String needle) {
-
         char[] char1 = haystack.toCharArray();
         char[] char2 = needle.toCharArray();
 
         int[] next = getNext(needle);
-        int i = 0;
-        int j = 0;
+        int i = 0,j=0;
 
-        while (i<char1.length&& j < char2.length){
+        while (i<char1.length&&j<char2.length){
             if (j==-1||char1[i]==char2[j]){
                 i++;
                 j++;
@@ -38,85 +38,25 @@ public class code28 {
             }
         }
 
-        return j ==char2.length?i-j:-1;
+        return j==char2.length?i-j:-1;
     }
 
     private static int[] getNext(String needle) {
-
-        char[] chars = needle.toCharArray();
+        int i =0;
+        int j = -1;
         int[] next = new int[needle.length()];
         next[0] = -1;
-        int j = 0;
-        int k = -1;
-        while (j<needle.length()-1){
-            if (k==-1||chars[j]==chars[k]){
+        while (i<needle.length()-1){
+            if (j==-1 ||needle.charAt(i)==needle.charAt(j)){
+                i++;
                 j++;
-                k++;
-                next[j]=k;
+                next[i] = j;
             }else {
-                k = next[k];
+                j = next[j];
             }
         }
         return next;
     }
-
-
-    // KMP算法
-//    public static int strStr3(String haystack, String needle) {
-//        char[] t = haystack.toCharArray();
-//        char[] p = needle.toCharArray();
-//        int i = 0; // 主串的位置
-//        int j = 0; // 模式串的位置
-//        int[] next = getNext(needle);
-//        while (i < t.length && j < p.length) {
-//            if (j == -1 || t[i] == p[j]) { // 当j为-1时，要移动的是i，当然j也要归0
-//                i++;
-//                j++;
-//            } else {
-//                j = next[j]; // j回到指定位置
-//            }
-//        }
-//        return j == p.length? i-j:-1;
-//    }
-//
-//    private static int[] getNext(String needle) {
-//        char[] chars =needle.toCharArray();
-//        int[] next = new int[chars.length];
-//        next[0] = -1;
-//        int j =  0;
-//        int k = -1;
-//        while (j<chars.length-1){
-//            if (k==-1 || chars[j]==chars[k]){
-//                j++;
-//                k++;
-//                next[j] = k;
-//            }else {
-//                k =next[k];
-//            }
-//        }
-//        return next;
-//    }
-
-
-//    abababca
-//     abababca
-//    private static int[] getNext(String ps) {
-//        char[] p = ps.toCharArray();
-//        int[] next = new int[p.length];
-//        next[0] = -1;
-//        int j = 0;
-//        int k = -1;
-//        while (j < p.length - 1) {
-//            if (k == -1 || p[j] == p[k]) {
-//                k++;
-//                j++;
-//                next[j] = k;
-//            } else {
-//                k = next[k];
-//            }
-//        }
-//        return next;
-//    }
 
 
 }
