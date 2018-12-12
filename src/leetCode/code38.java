f5dcbd8b3d40f5771ab53530a37febb9dbbe9f09
lxd <768265051@qq.com>
2018-12-12 19:20:24 +0800

    leetCode 38

---
+++ src/leetCode/code38.java
@@ -0,0 +1,41 @@
+package leetCode;
+
+//1.     1
+//2.     11
+//3.     21
+//4.     1211
+//5.     111221
+
+public class code38 {
+
+    public static void main(String[] args) {
+         int n = 4;
+         System.out.println(countAndSay(n));
+
+    }
+
+    private static String countAndSay(int n) {
+       
+    }
+
+
+
+
+    //        if (n <= 0) return "";
+//        string res = "1";
+//        while (--n) {
+//            string cur = "";
+//            for (int i = 0; i < res.size(); ++i) {
+//                int cnt = 1;
+//                while (i + 1 < res.size() && res[i] == res[i + 1]) {
+//                    ++cnt;
+//                    ++i;
+//                }
+//                cur += to_string(cnt) + res[i];
+//            }
+//            res = cur;
+//        }
+//        return res;
+
+
+}
