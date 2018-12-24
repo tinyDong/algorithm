d4ed996c1334c2be335251f18e3421795b01934e
lxd <768265051@qq.com>
2018-12-24 13:28:16 +0800

    leetCode 80 更灵活的写法

--- src/leetCode/code100/code80.java
+++ src/leetCode/code100/code80.java
@@ -1,5 +1,7 @@
 package leetCode.code100;
 
+import java.util.Arrays;
+
 //给定一个排序数组，你需要在原地删除重复出现的元素，使得每个元素最多出现两次，返回移除后数组的新长度。
 //
 //不要使用额外的数组空间，你必须在原地修改输入数组并在使用 O(1) 额外空间的条件下完成。
@@ -23,7 +25,32 @@ public class code80 {
     public static void main(String[] args) {
         int[] nums = new int[]{1,1,1,2,2,3};
 
-        System.out.println(removeDuplicates(nums));
+        System.out.println(removeDuplicates1(nums,2));
+        System.out.println(Arrays.toString(nums));
+
+    }
+
+    public static int removeDuplicates1(int[] nums,int length) {
+        int count =0 ;
+        int pre = 0;
+        int cur = 0;
+        while (cur < nums.length){
+            if (nums[pre]==nums[cur]&&count < length){
+                nums[pre+count] = nums[cur];
+                count++;
+                cur++;
+            }else {
+               if (nums[cur] == nums[cur-1] ){
+                    cur++;
+                }else {
+                    pre = pre+count;
+                    count=1;
+                    nums[pre] = nums[cur];
+                    cur++;
+                }
+            }
+        }
+        return pre+count;
     }
 
 
