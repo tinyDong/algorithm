70948910417b7133f33c9ef5ef6b494e8535ed07
lxd <768265051@qq.com>
2018-12-29 16:43:06 +0800

    leetcode 113

---
+++ src/leetCode/code150/code113.java
@@ -0,0 +1,17 @@
+package leetCode.code150;
+
+public class code113 {
+
+
+    public static void main(String[] args) {
+        
+    }
+
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode(int x) { val = x; }
+    }
+}
