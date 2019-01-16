7d920f51989f276548a213b03c3b4ca72eaf485e
lxd <768265051@qq.com>
2019-01-16 18:49:30 +0800

    leetcode 179

---
+++ src/leetCode/code200/code179.java
@@ -0,0 +1,26 @@
+package leetCode.code200;
+
+//示例 1:
+//
+//输入: [10,2]
+//输出: 210
+//示例 2:
+//
+//输入: [3,30,34,5,9]
+//输出: 9534330
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class code179 {
+    public static void main(String[] args) {
+        int[] nums = new int[]{10,2};
+        System.out.println(largestNumber(nums));
+    }
+
+
+    public static String largestNumber(int[] nums) {
+        List<Integer> res = new ArrayList<>();
+
+    }
+}
