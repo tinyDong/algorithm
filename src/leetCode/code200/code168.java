1bbd1e21e167e454c8bb9a369b632c4c2f1131a1
lxd <768265051@qq.com>
2019-01-16 14:30:38 +0800

    leetcode 168

--- src/leetCode/code200/code168.java
+++ src/leetCode/code200/code168.java
@@ -3,18 +3,18 @@ package leetCode.code200;
 public class code168 {
     public static void main(String[] args) {
 
-        System.out.println(convertToTitle(100));
+        System.out.println(convertToTitle(28));
     }
 
     public static String convertToTitle(int n) {
-        StringBuilder res = new StringBuilder();
+        StringBuilder sb = new StringBuilder();
         while (n!=0){
             n--;
-            char ch = (char)(n % 26 +'A');
+            char ch = (char) (n%26+'A');
+            sb.append(ch);
             n = n/26;
-            res.append(ch);
+            sb.reverse();
         }
-        res.reverse();
-        return res.toString();
+        return sb.toString();
     }
 }
