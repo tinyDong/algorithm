90d97af89616ff5f098ecf098c7a2225876e7d43
lxd <768265051@qq.com>
2019-01-17 11:13:40 +0800

    leetcode 179

--- src/leetCode/code200/code179.java
+++ src/leetCode/code200/code179.java
@@ -14,13 +14,23 @@ import java.util.List;
 
 public class code179 {
     public static void main(String[] args) {
-        int[] nums = new int[]{10,2};
+        int[] nums = new int[]{999999998,999999997,999999999};
         System.out.println(largestNumber(nums));
     }
 
 
     public static String largestNumber(int[] nums) {
-        List<Integer> res = new ArrayList<>();
+        List<Integer> list = new ArrayList<>();
+        for (int num : nums) {
+            list.add(num);
+        }
+        list.sort((o1, o2) -> (o2.toString()+o1.toString()).compareTo(o1.toString()+o2.toString()));
 
+        StringBuilder res = new StringBuilder();
+        for (Integer integer:list){
+            res.append(integer);
+        }
+
+        return res.toString().replaceAll("^(0+)\\B", "");
     }
 }
