a3857a9a64308d94aab9da91713f190af87ea88c
lxd <768265051@qq.com>
2019-01-22 14:40:14 +0800

    leetcode 209

--- src/leetCode/code250/code209.java
+++ src/leetCode/code250/code209.java
@@ -11,8 +11,65 @@ public class code209 {
     public static void main(String[] args) {
 
         int[] nums = new int[]{1,2,3,4,5};
-        System.out.println(minSubArrayLen(15,nums));
+        System.out.println(minSubArrayLen1(15,nums));
     }
+
+
+//    public static int minSubArrayLen2(int target, int[] nums) {
+//        int len = nums.length, res = len + 1 ;
+//        int[] sums = new int[len + 1];
+//        for (int i = 1; i < len + 1; ++i){
+//            sums[i] = sums[i - 1] + nums[i - 1];
+//        }
+//        for (int i = 0; i < len + 1; ++i) {
+//
+//            int right = searchRight(i + 1, len, sums[i] + target, sums);
+//
+//            if (right == len + 1){
+//                break;
+//            }
+//            if (res > right - i){
+//                res = right - i;
+//            }
+//        }
+//        return res == len + 1 ? 0 : res;
+//    }
+//
+//    private static int searchRight(int left, int right, int key, int sums[]) {
+//        while (left <= right) {
+//            int mid = (left + right) / 2;
+//            if (sums[mid] >= key){
+//                right = mid - 1;
+//            } else{
+//                left = mid + 1;
+//            }
+//        }
+//        return left;
+//    }
+
+
+
+    public static int minSubArrayLen1(int target, int[] nums) {
+        int left =0 , n = nums.length , res = n+1,sum =0;
+        for (int i = 0; i < n; i++) {
+            sum = sum + nums[i];
+            while (left<=i && sum>=target){
+                res = Math.min(res,i - left + 1);
+                sum = sum - nums[left];
+                left++;
+            }
+        }
+        return res==n+1?0:res;
+    }
+
+
+
+
+
+
+
+
+
 
 
     public static int minSubArrayLen(int target, int[] nums) {
