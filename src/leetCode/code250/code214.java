6cca2bb6009763e49c4eefa13fd06895df61bfd6
lxd <768265051@qq.com>
2019-01-23 11:43:36 +0800

    leetcode 214

--- src/leetCode/code250/code214.java
+++ src/leetCode/code250/code214.java
@@ -10,32 +10,49 @@ package leetCode.code250;
 
 //abbabaab
 //baababbabaab
+
+//1 求最长回文字符串 剩余部分补齐
+//2 字符串倒序  求2个字符串KMP匹配部分
 public class code214 {
     public static void main(String[] args) {
-        String str = "aabcd";
+        String str = "abaab";
         String result = shortestPalindrome(str);
         System.out.println(result);
     }
 //"baaabbabaab"
 //"baababbabaab"
     public static String shortestPalindrome(String s) {
+
+//        String r = new StringBuilder(s).reverse().toString();
+//        String t = s + "#" + r;
+//        System.out.println(t);
+//        int[] next = new int[t.length()];
+//
+//        for (int i = 1; i < t.length(); ++i) {
+//            int j = next[i - 1];
+//            while (j > 0 && t.charAt(i) != t.charAt(j))
+//            {
+//                j = next[j - 1];
+//            }
+//
+//            j =j + ((t.charAt(i) == t.charAt(j)) ? 1 : 0);
+//            next[i] = j;
+//        }
+//        return r.substring(0, s.length() - next[t.length() - 1]) + s;
+
         String r = new StringBuilder(s).reverse().toString();
         String t = s + "#" + r;
         System.out.println(t);
         int[] next = new int[t.length()];
-        
-        for (int i = 1; i < t.length(); ++i) {
-            int j = next[i - 1];
-            while (j > 0 && t.charAt(i) != t.charAt(j))
-            {
+        for (int i = 1; i < t.length(); i++) {
+            int j = next[i-1];
+            while (j>0 && t.charAt(i) != t.charAt(j)){
                 j = next[j - 1];
             }
-
-            j =j + ((t.charAt(i) == t.charAt(j)) ? 1 : 0);
+            j = j + (t.charAt(i)==t.charAt(j) ? 1:0);
             next[i] = j;
         }
-        return r.substring(0, s.length() - next[t.length() - 1]) + s;
-
+        return r.substring(0,r.length() - next[t.length()-1])+s;
     }
 
 }
