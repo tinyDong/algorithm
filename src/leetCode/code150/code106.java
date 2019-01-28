42315749920df6d1b862642c4a88ee8d5f58db0d
lxd <768265051@qq.com>
2019-01-28 18:05:55 +0800

    leetcode 106练习

--- src/leetCode/code150/code106.java
+++ src/leetCode/code150/code106.java
@@ -9,51 +9,36 @@ package leetCode.code150;
 //
 //中序遍历 inorder = [9,3,15,20,7]
 //后序遍历 postorder = [9,15,7,20,3]
-//返回如下的二叉树：
-//
-//3
-/// \
-//9  20
-///  \
-//15   7
 public class code106 {
 
     public static void main(String[] args) {
-
+        int[] nums1= new int[]{9,3,15,20,7};
+        int[] nums2= new int[]{9,15,7,20,3};
+        TreeNode res = buildTree(nums1,nums2);
+        System.out.println(res);
     }
 
     public static TreeNode buildTree(int[] inorder, int[] postorder) {
-        if(inorder.length == 0)
-            return null;
-        return helper(postorder,postorder.length-1,inorder,0,inorder.length-1);
+        return helper(inorder,postorder,postorder.length-1,0,inorder.length-1);
     }
 
-    private static TreeNode helper(int[] postorder, int postIndex, int[] inorder, int inStart, int inEnd) {
-        if (inStart>inEnd){
+    private static TreeNode helper(int[] inorder, int[] postorder,int postIndex ,int inStart,int inEnd) {
+
+        if (inStart > inEnd){
             return null;
         }
 
-        if (inStart == inEnd){
-            return new TreeNode(inorder[inStart]);
-        }
+        int rootValue = postorder[postIndex];
         int index = inStart;
-        while (index<inEnd && inorder[index]!=postorder[index]){
+        while (index < inEnd &&inorder[index] != rootValue){
             index++;
         }
-        TreeNode node = new TreeNode(postorder[postIndex]);
-        node.left = helper(postorder,postIndex -(inEnd-index)-1,inorder,inStart,index-1);
-        node.right = helper(postorder,postIndex-1,inorder,index+1,inEnd);
-        return node;
+
+        TreeNode root = new TreeNode(rootValue);
+        root.left = helper(inorder,postorder,postIndex-(inEnd-index)-1,inStart,index-1);
+        root.right = helper(inorder,postorder,postIndex-1,index+1,inEnd);
+        return root;
     }
 
 
-    public static class TreeNode {
-        int val;
-        TreeNode left;
-        TreeNode right;
-
-        TreeNode(int x) {
-            val = x;
-        }
-    }
 }
