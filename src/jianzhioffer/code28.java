9b35a61fc9317f28890f0d70bdf675faad6ab2a9
lxd <768265051@qq.com>
2019-02-13 18:17:23 +0800

    jianzhi code28

--- src/jianzhioffer/code28.java
+++ src/jianzhioffer/code28.java
@@ -6,8 +6,24 @@ public class code28 {
     public static void main(String[] args) {
         int[] nums =new int[]{1,2,3,2,2,2,5,4,2};
 
-        System.out.println(MoreThanHalfNum_Solution(nums));
+        System.out.println(MoreThanHalfNum_Solution1(nums));
     }
+    public static int MoreThanHalfNum_Solution1(int [] array) {
+        int result = array[0];
+        int count = 1;
+        for (Integer i : array){
+            if (result == i){
+                count++;
+            }else if (count==0){
+                result = i;
+                count ++;
+            }else {
+                count--;
+            }
+        }
+        return result;
+    }
+
 
     public static int MoreThanHalfNum_Solution(int [] array) {
         HashMap<Integer,Integer> map =new HashMap<>();
