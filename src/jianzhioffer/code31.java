dd340cfde1eee903298f883cf2a5b56fda85e9c8
lxd <768265051@qq.com>
2019-02-14 14:54:16 +0800

    jianzhi code31 略

---
+++ src/jianzhioffer/code31.java
@@ -0,0 +1,21 @@
+package jianzhioffer;
+
+public class code31 {
+    public static void main(String[] args) {
+
+    }
+
+//    public static int countDigitOne(int n) {
+//        int ones = 0;
+//        for (long m = 1; m <= n; m *= 10)
+//            ones += (n/m + 8) / 10 * m + (n/m % 10 == 1 ? n%m + 1 : 0);
+//        return ones;
+//    }
+
+    public int NumberOf1Between1AndN_Solution(int n) {
+//        int ones = 0;
+//        for (long m = 1; m <= n; m *= 10)
+//            ones += (n/m + 8) / 10 * m + (n/m % 10 == 1 ? n%m + 1 : 0);
+//        return ones;
+    }
+}
