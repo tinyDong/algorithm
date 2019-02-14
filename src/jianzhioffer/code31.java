67cc52ecaf92be08afd215aa1d57710e6c6b44fd
lxd <768265051@qq.com>
2019-02-14 14:54:24 +0800

    jianzhi code31 略

--- src/jianzhioffer/code31.java
+++ src/jianzhioffer/code31.java
@@ -12,10 +12,10 @@ public class code31 {
 //        return ones;
 //    }
 
-    public int NumberOf1Between1AndN_Solution(int n) {
+//    public int NumberOf1Between1AndN_Solution(int n) {
 //        int ones = 0;
 //        for (long m = 1; m <= n; m *= 10)
 //            ones += (n/m + 8) / 10 * m + (n/m % 10 == 1 ? n%m + 1 : 0);
 //        return ones;
-    }
+//    }
 }
