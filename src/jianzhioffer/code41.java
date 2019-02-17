3bdf770c2f28dcd5f445ff0d28a8bdcaf1a2c361
lxd <768265051@qq.com>
2019-02-17 22:49:28 +0800

    code 41 解法1 解法2需再看

---
+++ src/jianzhioffer/code41.java
@@ -0,0 +1,48 @@
+package jianzhioffer;
+
+import java.util.ArrayList;
+
+public class code41 {
+    public static void main(String[] args) {
+
+        System.out.println(FindContinuousSequence(100));
+    }
+
+    public static ArrayList<ArrayList<Integer>> FindContinuousSequence(int sum) {
+        ArrayList<ArrayList<Integer>> res = new ArrayList<>();
+
+        helper(res,new ArrayList<>(),0,sum);
+        return res;
+    }
+
+    private static void helper(ArrayList<ArrayList<Integer>> res, ArrayList<Integer> subList, int index, int target) {
+        if (getSum(subList) == target){
+            res.add(new ArrayList<>(subList));
+            return;
+        }
+
+        for (int i = index; i < target; i++) {
+            if (!subList.isEmpty()&& subList.get(subList.size()-1)!=i-1){
+                break;
+            }
+            subList.add(i);
+            if (getSum(subList)>target&&!subList.isEmpty()){
+                subList.remove(subList.size()-1);
+                break;
+            }
+
+            helper(res,subList,i+1,target);
+
+            if (!subList.isEmpty())
+            subList.remove(subList.size()-1);
+        }
+    }
+
+    private static int getSum(ArrayList<Integer> subList) {
+        int res = 0;
+        for (Integer integer : subList){
+            res += integer;
+        }
+        return res;
+    }
+}
