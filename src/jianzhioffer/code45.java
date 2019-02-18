e7e1ddaab9ec1b61306836ff52b14a360ee71132
lxd <768265051@qq.com>
2019-02-18 16:17:54 +0800

    jianzhi code45

--- src/jianzhioffer/code45.java
+++ src/jianzhioffer/code45.java
@@ -1,37 +1,36 @@
 package jianzhioffer;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
 
 public class code45 {
 
     public static void main(String[] args) {
-        int[] nums = new int[]{0,0,1,2,5};
+        int[] nums = new int[]{0,0,2,0,4};
         System.out.println(isContinuous(nums));
     }
 
     public static boolean isContinuous(int [] numbers) {
-
-        int catsCount =0;
-        ArrayList<Integer> others = new ArrayList<>();
-        for (int i = 0; i < numbers.length; i++) {
-            if (numbers[i]==0){
-                catsCount++;
-            }else {
-                others.add(numbers[i]);
-            }
-        }
-
-        if (others.get(others.size()-1)  - others.get(0)>numbers.length - catsCount +1){
+        if (numbers.length==0){
             return false;
         }
+        Arrays.sort(numbers);
         Set<Integer> set = new HashSet<>();
-        for (Integer integer : others){
-            if (!set.add(integer)){
-                return false;
+
+        ArrayList<Integer> others = new ArrayList<>();
+        for (int number : numbers) {
+            if (number != 0) {
+                others.add(number);
+                if (!set.add(number)){
+                    return false;
+                }
             }
         }
-        return true;
+
+        int left = others.get(others.size()-1);
+        int right =  others.get(0);
+        return !(left - right>4);
     }
 }
