14d66713a3002a6a19c0a80167d67bf507b8b215
lxd <768265051@qq.com>
2019-02-20 11:16:48 +0800

    jianzhi code50

--- src/jianzhioffer/code50.java
+++ src/jianzhioffer/code50.java
@@ -5,8 +5,8 @@ import java.util.Set;
 
 public class code50 {
     public static void main(String[] args) {
-        int[] nums = new int[]{2,3,1,0,2,5,3};
-        System.out.println(duplicate(nums,1,new int[3]));
+        int[] nums = new int[]{2,3,1,5,2,5,3};
+        System.out.println(duplicate1(nums,1,new int[3]));
     }
 
     public static int duplicate(int numbers[],int length,int [] duplication) {
@@ -17,5 +17,24 @@ public class code50 {
             }
         }
         return -1;
+    }
+
+
+    public static int duplicate1(int numbers[],int length,int [] duplication) {
+        for (int i = 0; i < numbers.length; i++) {
+            while (i!=numbers[i]){
+                if (numbers[i] == numbers[numbers[i]]){
+                    return numbers[i];
+                }
+                swap(numbers,i,numbers[i]);
+            }
+        }
+        return -1;
+    }
+
+    private static void swap(int[] array,int start,int end){
+        int temp = array[start];
+        array[start] = array[end];
+        array[end] = temp;
     }
 }
